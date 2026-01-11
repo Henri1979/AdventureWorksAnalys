11b76be30bebef7f83e331ae6b0304ffcbf87a69
Henri Hörkkö <henri.h1979@gmail.com>
2026-01-11 21:29:18 +0100

    added vis2 försäljning per produktkategori

---
+++ data/vis2.sql
@@ -0,0 +1,23 @@
+USE AdventureWorks
+
+
+
+SELECT * FROM Production.ProductCategory
+SELECT * FROM Production.ProductSubcategory
+SELECT * FROM Production.Product
+SELECT * FROM Sales.SalesOrderDetail
+
+
+
+SELECT
+    pc.Name AS CategoryName,
+    SUM(sod.OrderQty * sod.UnitPrice) AS TotalRevenue
+FROM Production.ProductCategory pc
+INNER JOIN Production.ProductSubcategory psc 
+    ON pc.ProductCategoryID = psc.ProductCategoryID
+INNER JOIN Production.Product p 
+    ON psc.ProductSubcategoryID = p.ProductSubcategoryID
+INNER JOIN Sales.SalesOrderDetail sod
+    ON sod.ProductID = p.ProductID
+GROUP BY pc.Name
+ORDER BY TotalRevenue DESC
